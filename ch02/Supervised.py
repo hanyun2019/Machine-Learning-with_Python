2dee91d1a4724f75c33b5641ff05167e44409e4d
黄祉涵 <hanyun2019@gmail.com>
2019-10-02 22:27:34 +0800

    update for Linear Regression & Ridge Regression

--- ch02/Supervised.py
+++ ch02/Supervised.py
@@ -10,6 +10,8 @@ from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 
 ### Supervised Learning
 ## 2.1 Classification and Regression
@@ -257,7 +259,7 @@ print("\nKNN wave dataset regression example - y_test:\n", y_test)
 # Now we can make predictions on the test set
 print("\nKNN wave dataset regression example - test set predictions:\n", reg.predict(X_test))
 
-# The $R^2$ score, also known as coefficient of determination, 
+# The R^2 score, also known as coefficient of determination, 
 # is a measure of goodness of a prediction for a regression model, and yields a score up to 1. 
 # A value of 1 corresponds to a perfect prediction, and a value of 0 corresponds to a constant model 
 # that just predicts the mean of the training set responses y_train.
@@ -305,6 +307,134 @@ axes[0].legend(["Model predictions", "Training data/target",
 
 ## 2.4 Linear models
 # 2.4.1 Linear models for regression
+# y = w[0]*x[0] + b
+# Trying to learn the parameters w[0] and b on our one-dimensional wave dataset:
+# mglearn.plots.plot_linear_regression_wave()
+
+# Linear models for regression can be characterized as regression models for which the prediction is a line for a single feature, 
+# a plane when using two features, or a hyperplane in higher dimensions (that is when having more features).
+# 用于回归的线性模型：对于单一特征的预测结果是直线，两个特征时是平面，在更高维度（即更多特征）时是超平面
+
+# Linear regression aka ordinary least squares 线性回归（普通最小二乘法）
+
+# from sklearn.linear_model import LinearRegression
+print("\n----------- LinearRegression performs on wave dataset example -----------")
+
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+lr = LinearRegression().fit(X_train, y_train)
+
+# The “slope” parameters w, also called weights or coefficients are stored in the coef_ attribute, 
+# while the offset or intercept b is stored in the intercept_ attribute. 
+print("lr.coef_:", lr.coef_)      # The “slope” parameters  斜率参数
+print("lr.intercept_:", lr.intercept_)      # the offset or intercept b   偏移量或截距参数
+
+# you might notice the strange-looking trailing underscore. Scikit-learn alwaysstores anything that is derived from the 
+# training data in attributes that end with a trailing underscore. 
+# That is to separate them from parameters that are set by the user.
+# scikit-learn将从训练数据中得出的值保存在以下划线结尾的属性中，以此与用户设置的数据分开
+
+print("\nLinearRegression - wave dataset training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("\nLinearRegression - wave dataset test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+# Result: 
+# LinearRegression - wave dataset training set score: 0.67
+# LinearRegression - wave dataset test set score: 0.66
+# An R^2 of around .66 is not very good, but we can see that the score on training and test set are very close together. 
+# This means we are likely underfitting, not overfitting. 
+# For this one-dimensional dataset, there is little danger of overfitting, as the model is very simple (or restricted).
+
+
+# LinearRegression performs on Boston Housing dataset
+# Let’s take a look at how LinearRegression performs on a more complex dataset, like the Boston Housing dataset. 
+# Remember that this dataset has 506 samples and 105 derived features.
+# We load the dataset and split it into a training and a test set. Then we build the linear regression model as before:
+print("\n----------- LinearRegression performs on Boston Housing dataset example -----------")
+
+X, y = mglearn.datasets.load_extended_boston()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+lr = LinearRegression().fit(X_train, y_train)
+print("\nLinearRegression - Boston Housing dataset training set score: {:.2f}".format(lr.score(X_train, y_train)))
+print("\nLinearRegression - Boston Housing dataset test set score: {:.2f}".format(lr.score(X_test, y_test)))
+
+# Result: 
+# LinearRegression - Boston Housing dataset training set score: 0.95
+# LinearRegression - Boston Housing dataset test set score: 0.61
+# When comparing training set and test set score, we find that we predict very accurately on the training set, 
+# but the R^2 on the test set is much worse.
+
+# This is a clear sign of overfitting, and therefore we should try to find a model that allows us to control complexity.
+# One of the most commonly used alternatives to standard linear regression is Ridge regression, which we will look into next.
+
+
+# 2.4.2 Ridge regression (l2 regularization)
+# Ridge regression is also a linear model for regression, so the formula it uses to make predictions is still Formula (1), as for ordinary least squares. 
+# In Ridge regression,the coefficients w are chosen not only so that they predict well on the training data, but there is an additional constraint. 
+# We also want the magnitude of coefficients to be as small as possible; in other words, all entries of w should be close to 0.
+
+# Intuitively, this means each feature should have as little effect on the outcome as possible (which translates to having a small slope), 
+# while still predicting well.
+
+# This constraint is an example of what is called regularization. Regularization means explicitly restricting a model to avoid overfitting. 
+# The particular kind used by Ridge regression is known as l2 regularization.
+# 岭回归选择的斜率系数w很小（接近于0），这是一种正则化：L2正则化
+
+print("\n----------- Ridge regression performs on Boston Housing dataset example -----------")
+
+# from sklearn.linear_model import Ridge
+
+ridge = Ridge().fit(X_train, y_train)
+print("\nRidge regression(alpha=1.0) - Boston Housing dataset training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("\nRidge regression(alpha=1.0) - Boston Housing dataset test set score: {:.2f}".format(ridge.score(X_test, y_test)))
+
+# Result: 
+# Ridge regression - Boston Housing dataset training set score: 0.89
+# Ridge regression - Boston Housing dataset test set score: 0.75
+
+# As you can see, the training set score of Ridge is lower than for LinearRegression, while the test set score is higher. 
+# With linear regression, we were overfitting to our data. Ridge is a more restricted model, so we are less likely to overfit. 
+# A less complex model means worse performance on the training set, but better generalization.
+# As we are only interested in generalization performance, we should choose the Ridge model over the LinearRegression model.
+# Ridge在训练集上的分数要低于LinearRegression，但在测试集上的分数更高。线性回归对数据存在过拟合，Ridge是一种约束性更强的模型，所以不容易过拟合。
+# 由于我们只对模型的泛化性能感兴趣，所以更应该选择Ridge模型。
+
+# The Ridge model makes a trade-off between the simplicity of the model (near zero coefficients) and its performance on the training set. 
+# How much importance the model places on simplicity versus training set performance can be specified by the user, using the alpha parameter. 
+# Above, we used the default parameter alpha=1.0. 
+
+# Increasing alpha forces coefficients to move more towards zero, 
+# which decreases training set performance, but might help generalization.
+# Ridge模型的简单性和训练性能二者对于模型的重要程度，可以由用户通过设置alpha参数来指定。
+# 增大alpha会使系数更加趋向于0，从而降低训练集性能，但可能可以提高模型的泛化性能。
+
+# alpha=10
+ridge10 = Ridge(alpha=10).fit(X_train, y_train)
+print("\nRidge regression(alpha=10) - Boston Housing dataset training set score: {:.2f}".format(ridge10.score(X_train, y_train)))
+print("\nRidge regression(alpha=10) - Boston Housing dataset test set score: {:.2f}".format(ridge10.score(X_test, y_test)))
+
+# alpha=0.1
+ridge01 = Ridge(alpha=0.1).fit(X_train, y_train)
+print("\nRidge regression(alpha=0.1) - Boston Housing dataset training set score: {:.2f}".format(ridge01.score(X_train, y_train)))
+print("\nRidge regression(alpha=0.1) - Boston Housing dataset test set score: {:.2f}".format(ridge01.score(X_test, y_test)))
+
+# Result:
+# Ridge regression(alpha=1.0) - Boston Housing dataset training set score: 0.89
+# Ridge regression(alpha=1.0) - Boston Housing dataset test set score: 0.75
+
+# Ridge regression(alpha=10) - Boston Housing dataset training set score: 0.79
+# Ridge regression(alpha=10) - Boston Housing dataset test set score: 0.64
+
+# Ridge regression(alpha=0.1) - Boston Housing dataset training set score: 0.93
+# Ridge regression(alpha=0.1) - Boston Housing dataset test set score: 0.77
+
+# 小结：
+# 1）无论是岭线性回归还是普通线性回归，所有数据集大小对应的训练分数都要高于测试分数；
+# 2）由于岭回归是正则化的，因此它的训练分数要整体低于普通线性回归的训练分数；但其测试分数会更高，特别是对于较小的子数据集；
+# 3）如果少于400个数据点，线性回归学不到任何内容，随着模型可用的数据越来越多，两个模型的性能都会提升，最终普通线性回归的性能追上岭回归；
+# 4）如果有足够多的训练数据，正则化变得不那么重要，并且岭回归和普通线性回归将具有相同的性能；
+# 5）如果添加更多数据，线性回归的训练性能将下降，模型将更加难以过拟合或记住所有数据。
+
 
 
 
